c18b198b4ef72d9ce037d9523019d727b527fa24
김승규 <rlatmdrb1996@gmail.com>
2022-08-15 18:50:26 +0900

    build: 프로젝트 생성

---
+++ src/test/java/com/kkamjidot/api/mono/MonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkamjidot.api.mono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
