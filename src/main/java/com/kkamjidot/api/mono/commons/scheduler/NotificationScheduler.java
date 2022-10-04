af5561488495bb5280e5f41d2fea0c69585523f5
김승규 <rlatmdrb1996@gmail.com>
2022-10-05 03:34:50 +0900

    feat: 제거된 토큰인지 검사 추가(LEAR-277)

--- src/main/java/com/kkamjidot/api/mono/commons/scheduler/NotificationScheduler.java
+++ src/main/java/com/kkamjidot/api/mono/commons/scheduler/NotificationScheduler.java
@@ -7,10 +7,13 @@ import com.kkamjidot.api.mono.domain.enumerate.ApplicationStatus;
 import com.kkamjidot.api.mono.dto.NotificationRequest;
 import com.kkamjidot.api.mono.repository.ChallengeRepository;
 import com.kkamjidot.api.mono.repository.CompleteRepository;
+import com.kkamjidot.api.mono.repository.NotiTokenRepository;
 import com.kkamjidot.api.mono.repository.TakeAClassRepository;
 import com.kkamjidot.api.mono.service.NotificationService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.support.CronExpression;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,34 +21,37 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 @Transactional(readOnly = true)
 public class NotificationScheduler {
     private final ChallengeRepository challengeRepository;
-    private final TakeAClassRepository takeAClassRepository;
     private final CompleteRepository completeRepository;
     private final NotificationService notificationService;
+    private final NotiTokenRepository notiTokenRepository;
 
-    public NotificationScheduler(ChallengeRepository challengeRepository, TakeAClassRepository takeAClassRepository, CompleteRepository completeRepository, NotificationService notificationService) {
-        this.challengeRepository = challengeRepository;
-        this.takeAClassRepository = takeAClassRepository;
-        this.completeRepository = completeRepository;
-        this.notificationService = notificationService;
-    }
+//    public NotificationScheduler(ChallengeRepository challengeRepository, CompleteRepository completeRepository, NotificationService notificationService) {
+//        this.challengeRepository = challengeRepository;
+//        this.completeRepository = completeRepository;
+//        this.notificationService = notificationService;
+//    }
 
 
-//    @Scheduled(cron = "0/10 * * * * ?")      // 테스트용 매 분 실행
+//    @Scheduled(cron = "0/3 * * * * ?")      // 테스트용
     @Scheduled(cron = "0 0 19 ? * FRI,SAT")
     @Scheduled(cron = "0 0 15 ? * SUN")
     public void SendDeadlineNotification() {
+        // 알림 전송할 유저
         Set<User> sendUserSet = new LinkedHashSet<>();
 
+        // 진행 중인 챌린지 리스트
         List<Challenge> runningChallengeList = challengeRepository.findAllByRunning(LocalDateTime.now(ZoneId.of("Asia/Seoul")));
 
+        // 챌린지 별 이번 주 아직 미션을 완료하지 않은 인원들 조회
         runningChallengeList.forEach(challenge -> {
             Integer thisWeek = challenge.getThisWeek();
             List<User> userList = challenge.getTakeAClasses().stream()
@@ -58,14 +64,27 @@ public class NotificationScheduler {
             });
         });
 
-        sendUserSet.forEach(user -> user.getNotiTokens().forEach(token -> {
-            NotificationRequest notificationRequest = NotificationRequest.builder()
-                    .token(token.getTokenValue())
-                    .title("깜지")
-                    .message("이번 주 배운 내용을 깜지에 기록해보세요!")
-                    .build();
+        String[] notificationContent = new String[]{"이번 주 배운 내용을 깜지에 기록해보세요!", "우리 깜지에서 같이 공부해봐요🤗"};
 
-            notificationService.sendNotification(notificationRequest);
+        // 알림 메시지 발송
+        sendUserSet.forEach(user -> user.getNotiTokens().forEach(token -> {
+            if (token.getTokenDeletedDate() == null) {
+                NotificationRequest notificationRequest = NotificationRequest.builder()
+                        .token(token.getTokenValue())
+                        .title("깜지")
+                        .message(notificationContent[new Random().nextInt(notificationContent.length)])
+                        .build();
+                notificationService.sendNotification(notificationRequest);
+            }
         }));
+    }
+
+
+//    @Scheduled(cron = "0/3 * * * * ?")      // 테스트용
+    @Scheduled(cron = "0 0 9-18 ? * ?")
+    public void SendCustomNotification() {
+        for (String s : notiTokenRepository.findById(2L).get().getTokenValue().split("\n")) {
+            System.out.println("<<>>" + s);
+        }
     }
 }
