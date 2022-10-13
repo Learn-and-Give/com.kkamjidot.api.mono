f46ed86e2002f99e76c79721f91dda88666b894c
김승규 <rlatmdrb1996@gmail.com>
2022-10-13 10:56:08 +0900

    feat: 퀴즈 평가 API 인가 변경(LEAR-331)

--- src/main/java/com/kkamjidot/api/mono/repository/TakeAClassRepository.java
+++ src/main/java/com/kkamjidot/api/mono/repository/TakeAClassRepository.java
@@ -4,13 +4,8 @@ import com.kkamjidot.api.mono.domain.Challenge;
 import com.kkamjidot.api.mono.domain.TakeAClass;
 import com.kkamjidot.api.mono.domain.User;
 import com.kkamjidot.api.mono.domain.enumerate.ApplicationStatus;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
