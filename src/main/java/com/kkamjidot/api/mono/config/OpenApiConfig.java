479663505e61c5a1925b358afdd9ebfeef8c2d1c
김승규 <rlatmdrb1996@gmail.com>
2022-10-13 18:01:18 +0900

    doc: api 명세 최신화

--- src/main/java/com/kkamjidot/api/mono/config/OpenApiConfig.java
+++ src/main/java/com/kkamjidot/api/mono/config/OpenApiConfig.java
@@ -19,8 +19,8 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
         Info info = new Info()
-                .title("깜지. API 서버 V1")
-                .version("v2.3.7")
+                .title("깜지. API 서버")
+                .version("v2.3.8")
                 .description("학습자료 공유 기반 챌린지 서비스 플랫폼 깜지.의 API 통합 서버 (아래 README 사이트 참고)")
                 .contact(new Contact().name("README").url("https://github.com/Learn-and-Give/com.kkamjidot.api.mono/tree/develop#readme"));
 
